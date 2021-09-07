545438896a2f039ed2049afc58db3ca99e2e0147
Arish Khan <arishsultan104@gmail.com>
2021-09-07 13:33:31 +0500

    Added a [TimedWidget] to ease making widgets that need to update periodically like, stop watch.

---
+++ lib/widgets/timer_widget.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:reusables/reusables.dart';
+
+typedef TimedWidgetBuilder = Function(BuildContext context, int tick);
+
+class TimedWidgetController extends ChangeNotifier {
+  Timer? _timer;
+  Timer? _timerFor;
+
+  void start(Duration period, [Duration? runFor]) {
+    if (_timer != null) {
+      throw 'a timer is active already stop the previous timer to start again';
+    }
+
+    if (runFor != null) {
+      _timerFor = Timer(runFor, cancel);
+    }
+
+    _timer = Timer.periodic(period, _notifyListeners);
+  }
+
+  void cancel() {
+    if (_timerFor?.isActive == true) {
+      _timerFor?.cancel();
+      _timerFor = null;
+    }
+
+    if (_timer?.isActive == true) {
+      _timer?.cancel();
+      _timer = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_timer?.isActive == true) {
+      _timer?.cancel();
+      _timer = null;
+    }
+    super.dispose();
+  }
+
+  void _notifyListeners(Timer _) => notifyListeners();
+}
+
+class TimedWidget extends ControlledWidget<TimedWidgetController> {
+  const TimedWidget({
+    Key? key,
+    required this.builder,
+    required TimedWidgetController controller,
+  }) : super(key: key, controller: controller);
+
+  final TimedWidgetBuilder builder;
+
+  @override
+  _TimedWidgetState createState() => _TimedWidgetState();
+}
+
+class _TimedWidgetState extends State<TimedWidget> {
+  @override
+  Widget build(BuildContext context) =>
+      widget.builder(context, widget.controller._timer?.tick ?? 0);
+}
