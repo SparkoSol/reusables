4367eb83c5e3c7c135538358d1b4fd2ed2e573a2
Arish Khan <arishsultan104@gmail.com>
2021-09-07 13:37:27 +0500

    exported `timed_widget.dart`

--- lib/reusables.dart
+++ lib/reusables.dart
@@ -1,7 +1,6 @@
 library blink_flutter_reuseables;
 
 export 'mixins/app_locale_config.dart';
-
 export 'services/auth_session.dart';
-
 export 'widgets/controlled_widget.dart';
+export 'widgets/timed_widget.dart';
