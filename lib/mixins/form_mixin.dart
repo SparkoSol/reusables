f72c44c7198d7191a43cf9a941da92e80aecb821
Arish Khan <arishsultan104@gmail.com>
2021-09-17 22:24:30 +0500

    added [FormMixin] to ease use of Forms

---
+++ lib/mixins/form_mixin.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+
+/// Add this mixin to a [Widget] or [State] to ease use of [Form].
+mixin FormMixin on Widget, State {
+  /// This Key must be bound with the [Form] Widget that is to be used.
+  final formKey = GlobalKey<FormState>();
+
+  /// Pass this function as a parameter to Widget that acts as a submit button,
+  ///
+  /// For Example,
+  /// ```dart
+  /// TextButton(
+  ///   child: Text('Submit'),
+  ///   onPressed: submitter,
+  /// )
+  /// ```
+  void submitter() {
+    // First check if all the fields are valid or not i.e. provided validators.
+    if (formKey.currentState?.validate() == true) {
+      // Save all the field values i.e. data binding.
+      formKey.currentState?.save();
+
+      // Call user defined code.
+      onSubmit();
+    }
+  }
+
+  /// The user must override this method since this method will be called by
+  /// [submitter] once the form is validated.
+  FutureOr<void> onSubmit() {}
+}
--- lib/reusables.dart
+++ lib/reusables.dart
@@ -1,5 +1,6 @@
 library blink_flutter_reuseables;
 
+export 'mixins/form_mixin.dart';
 export 'mixins/app_locale_config.dart';
 export 'services/auth_session.dart';
 export 'widgets/controlled_widget.dart';
