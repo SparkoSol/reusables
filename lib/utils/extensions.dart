d17edd142cb0ef2426e3f3e8db80398580ba65aa
Rashid-Khabeer <71066995+Rashid-Khabeer@users.noreply.github.com>
2022-05-07 17:01:49 +0500

    Added date extension

---
+++ lib/utils/extensions.dart
@@ -0,0 +1,18 @@
+import 'package:intl/intl.dart';
+
+extension DateFormatter on DateTime {
+  /// Date Format to 05-07-2022
+  String get dateFormat => DateFormat('dd-MM-yyyy').format(this);
+
+  /// Time Format to 04:49 PM
+  String get timeFormat => DateFormat('hh:mm a').format(this);
+
+  /// Time Format to 05-07-2022 04:49 PM
+  String get dateTimeFormat => DateFormat('dd-MM-yyyy, hh:mm a').format(this);
+
+  /// Time Format to 05 Feb 2022
+  String get dateMonthFormat => DateFormat('dd MMM yyyy').format(this);
+
+  /// Time Format to 05-07-2022 Saturday
+  String get dateDayFormat => DateFormat('dd-MM-yyyy EEEE').format(this);
+}
