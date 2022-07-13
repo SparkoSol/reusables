87cb449468f23dc64762a9363b29ca4ae3bb8f55
arish <arishsultan104@gmail.com>
2022-07-14 00:38:07 +0500

    changed version and exported [EventControlledWidget].

--- lib/reusables.dart
+++ lib/reusables.dart
@@ -5,6 +5,7 @@ export 'mixins/app_locale_config.dart';
 
 export 'services/auth_session.dart';
 
+export 'widgets/event_controlled_widget.dart';
 export 'widgets/controlled_widget.dart';
 export 'widgets/timed_widget.dart';
 
